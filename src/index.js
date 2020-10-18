b52bc82436a2cf32999945b994ea631057c8583b
Mathieu Lallemand (lalmat) <2410437+lalmat@@users.noreply.github.com>
2020-10-18 13:44:18 +0200

    feat: domoticz connected

--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
+import Axios from "axios";
+
 class API {
 
-    constructor(options) {
-        this.options = {
-            host     : options.host     ? options.host     : null,
-            username : options.username ? options.username : null,
-            password : options.password ? options.password : null,
-        };
+    constructor(hostname, options) {
+        this.devices = {};
+
+        this.useSSL = options.useSSL ? options.useSSL : false;
+
+        this.domoticzUrl  = this.useSSL ? "https://" : "http://";
+        this.domoticzUrl += hostname;
+
+        if (options.port) {
+            this.domoticzUrl += `:${options.port}`;
+        }
+
+        this.domoticzUrl += "/json.htm?_connector=domoticz-api";
+
+        if (options.username && options.password) {
+            this.domoticzUrl += `&username=${btoa(options.username)}`;
+            this.domoticzUrl += `&password=${btoa(options.password)}`;
+        }
+
+        this.domoticzUrl += "&";
     }
 
-    getOptions() {
-        return this.options;
+    async version() {
+        let uri = "type=command&param=getversion";
+        return await this.domoticzCall(uri);
+    }
+
+    async log(message) {
+        message = "----------- DOMOTICZ API -----------> " + message;
+        let uri = `type=command&param=addlogmessage&message=${encodeURI(message)}`;
+        return await this.domoticzCall(uri);
+    }
+
+    async domoticzCall(order) {
+        try {
+            let result = await Axios.get(this.domoticzUrl+order);
+            if (result.data.status === "OK") return result.data;
+            return null;
+        }
+        catch (e) {
+            console.log("Domotiz API Query Error: ", e);
+        }
     }
 }
 
